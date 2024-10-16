76deca0c03ca8141040aa155170474df7366e897
ghagevaibhav <ghagevaibhav@gmail.com>
2024-10-16 17:46:42 +0530

    updateComic endpoint done

--- routes/comic.js
+++ routes/comic.js
@@ -11,7 +11,7 @@ const comicBody = zod.object({
     discount: zod.number().min(0).max(100),
     numberOfPages: zod.number(),
     condition: zod.string(),
-    description: zod.string().isOptional(),
+    description: zod.string().isOptional(true),
 })
 
 router.post('/createComic', async (req, res) => {
@@ -40,7 +40,46 @@ router.post('/createComic', async (req, res) => {
     res.status(201).json({message: 'Comic Created'});
 })
 
-router.put('/updateComic', (req, res) => {
-    
+const comicUpdateBody = zod.object({
+    bookName: zod.string().optional(),
+    authorName: zod.string().optional(),
+    yearOfPublication: zod.number().optional(),
+    price: zod.number().optional(),
+    discount: zod.number().min(0).max(100).optional(),
+    numberOfPages: zod.number().optional(),
+    condition: zod.string().optional(),
+    description: zod.string().optional(),
 })
 
+
+router.put('/updateComic/:id', async (req, res) => {
+        const { success } = comicUpdateBody.safeParse(req.body);
+
+        if(!success){
+            return res.status(400).json({message: 'Incorrect Inputs Provided'});
+        }
+
+        const comic = await Comic.findOne({_id: req.params.id});
+
+        if(!comic){
+            return res.status(404).json({message: 'Comic Not in Database'});
+        }
+
+        await comic.updateOne({
+            $set: {
+                bookName: req.body.bookName || comic.bookName,
+                authorName: req.body.authorName || comic.authorName,
+                yearOfPublication: req.body.yearOfPublication || comic.yearOfPublication,
+                price: req.body.price || comic.price,
+                discount: req.body.discount || comic.discount,
+                numberOfPages: req.body.numberOfPages || comic.numberOfPages,
+                condition: req.body.condition || comic.condition,
+                description: req.body.description || comic.description,
+            }
+        })
+
+        return res.status(201).json({
+            message: 'Comic Updated',
+            updatedComic: Comic.findOne({_id: req.params.id}),
+        })
+})
